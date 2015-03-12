8974ef23906bc5dda74f024f549e94916d37aea5
MiniBear <lei.zhang.bupt08@gmail.com>
2015-03-12 22:08:27 +0800

    ADD:增加functions.php文件

--- header.php
+++ header.php
@@ -1,47 +1,17 @@
-<!-- <!DOCTYPE <html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>SkyStar Studio</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="SkyStar Studio Development Team Website">
-		<meta name="author" content="SkyStar Studio">
-
-		Le styles
-		<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-		<style type="text/css">
-			body {
-				padding-top: 60px;
-				padding-bottom: 40px;
-			}
-		</style>
-		<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap-responsive.css">
-	</head>
-<body>
-	<div class="container"> -->
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Bootstrap, from Twitter</title>
+    <title>SkyStar Studio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="An amazing development team.">
+    <meta name="author" content="SkyStar Studio">
 
     <!-- Le styles -->
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>">
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
 
-    <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="../assets/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    <?php wp_enqueue_script("jquery"); ?>
+    <?php wp_head(); ?>
   </head>
 
   <body>
